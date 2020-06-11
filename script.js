5a355461ac69280912bdba599f561f23d2edc02c
Marcio Filadelfo <marcio.kabana@gmail.com>
2020-06-11 12:18:44 -0300

    Update script.js

--- script.js
+++ script.js
@@ -62,7 +62,7 @@ function desenho(){
     //areaDesenho.font("20px Arial");
     areaDesenho.fillText("Humano: " + pontosPlayer1 + " pontos", 100, 100);
     areaDesenho.fillText("Computador: " + pontosPlayer2 + " pontos", larguraCampo - 100, 100);
-    areaDesenho.fillText("Kabana Devs Production", larguraCampo - 100, 700);
+    areaDesenho.fillText("Kabana Devs Production", larguraCampo - 120, alturaCampo - 20);
 }
 function reiniciar(){
     bolaX = larguraCampo / 2;
